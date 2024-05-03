7752e9f4f4b59798abb3bfb5bd824763e5db7080
Piotr Jurga <deutor1361@gmail.com>
2024-05-03 21:42:39 +0200

    encrypted communication - *** warning! app accepts any self-signed cert ***

--- app/src/main/java/eu/ldaldx/mobile/zscanner/RestClient.java
+++ app/src/main/java/eu/ldaldx/mobile/zscanner/RestClient.java
@@ -1,9 +1,12 @@
 package eu.ldaldx.mobile.zscanner;
 
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import android.annotation.SuppressLint;
+
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
@@ -16,22 +19,25 @@ public class RestClient {
     private static Retrofit retrofit = null;
     private static RestApi restApi = null;
 
-    // Create a custom TrustManager that accepts all certificates
-    TrustManager[] trustAllCerts = new TrustManager[]{
-            new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
+    // Create a custom TrustManager that accepts all certificates - this will accept any self-signed certificate
+    // however this app is designed to be used in trusted LAN environments
+    // we accept self-signed cert from app server (zserver4mc) - if you need secure connection you need issue valid cert to zeserver4mc and pin it to this app
+    //@SuppressLint("CustomX509TrustManager") - not suppressing that warning, so you have chance to see security warning
+    TrustManager trustAllCerts = new X509TrustManager() {
+        @SuppressLint("TrustAllX509TrustManager")
+        @Override
+        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+        }
 
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
+        @SuppressLint("TrustAllX509TrustManager")
+        @Override
+        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+        }
 
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            }
+        @Override
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return new java.security.cert.X509Certificate[] {};
+        }
     };
 
 
@@ -41,35 +47,41 @@ public class RestClient {
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-            /*
+            /**/
             SSLContext sslContext;
             try {
-                sslContext = SSLContext.getInstance("TLS");
-                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            } catch(Exception ex) {
+                sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, new TrustManager[] {trustAllCerts}, new java.security.SecureRandom());
 
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .addInterceptor(interceptor)
+                        .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts)
+                        .readTimeout(60, TimeUnit.SECONDS)
+                        .connectTimeout(10, TimeUnit.SECONDS)
+                        .hostnameVerifier(new HostnameVerifier() {
+                            // we already accept self-signed cert from app server (zserver4mc) - if you need secure connection you need issue valid cert to zeserver4mc and pin it to this app
+                            @Override
+                            public boolean verify(String hostname, SSLSession session) {
+                                return true;
+                            }
+                        })
+                        .build();
+
+
+
+                retrofit = new Retrofit.Builder()
+                        .baseUrl("https://" + server + ":" + port)
+                        .addConverterFactory(MoshiConverterFactory.create())
+                        .client(client)
+                        .build();
+
+                restApi = retrofit.create(RestApi.class);
+
+            } catch(Exception ex) {
+                throw new RuntimeException(ex.toString());
             }
 
-             */
 
-
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(interceptor)
-            //        .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0])
-                    .readTimeout(60, TimeUnit.SECONDS)
-                    .connectTimeout(10, TimeUnit.SECONDS)
-                    .build();
-
-
-
-
-            retrofit = new Retrofit.Builder()
-                    .baseUrl("http://" + server + ":" + port)
-                    .addConverterFactory(MoshiConverterFactory.create())
-                    .client(client)
-                    .build();
-
-            restApi = retrofit.create(RestApi.class);
         }
 
         return retrofit;
